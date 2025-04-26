47de1c65f6010d74bbd561179681f600130998f5
Rafael Ribiero <ribeirorafaelmatheus@gmail.com>
2025-04-26 19:54:39 -0300

    feat: Implement graceful shutdown for HTTP server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,14 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/rafaelmgr12/litegodb/pkg/litegodb"
 	"github.com/rs/cors"
@@ -41,6 +46,34 @@ func (s *Server) Start() error {
 		handler = cors.Default().Handler(handler)
 	}
 
-	log.Printf("Starting server on %s...", addr)
-	return http.ListenAndServe(addr, handler)
+	server := &http.Server{
+		Addr:    addr,
+		Handler: handler,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Gracefully shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	if err := s.DB.Close(); err != nil {
+		log.Printf("Error closing DB: %v", err)
+	}
+
+	log.Println("Server exited properly")
+	return nil
 }
